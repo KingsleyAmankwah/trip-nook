2f4d65c2f1acd60e6150cfd4d4dbc43d1d5ba714
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-10-02 07:45:17 +0000

    current user added to user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,9 +7,12 @@ import MenuItem from "./MenuItem";
 import { useRouter } from "next/navigation";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
 
 interface UserMenuProps {
-  currentUser?: null;
+  currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
@@ -37,7 +40,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         >
           <AiOutlineMenu className="text-2xl" />
           <div className="hidden md:block">
-            <Avatar />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
@@ -63,9 +66,9 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   label="My properties"
                   onClick={() => router.push("/properties")}
                 />
-                {/* <MenuItem label="Airbnb your home" onClick={rentModal.onOpen} /> */}
+                <MenuItem label="Airbnb your home" onClick={() => {}} />
                 <hr />
-                {/* <MenuItem label="Logout" onClick={() => signOut()} /> */}
+                <MenuItem label="Logout" onClick={() => signOut()} />
               </>
             ) : (
               <>
