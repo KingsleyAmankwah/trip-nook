184ccf526b32b773a9ff44fb8c80820178065af7
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-10-04 18:55:39 +0000

    Loader component

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import { PuffLoader } from "react-spinners";
+import { PuffLoader } from "react-spinners";
 
 const Loader = () => {
   return (
@@ -13,10 +13,7 @@ const Loader = () => {
       items-center 
     "
     >
-      {/* <PuffLoader
-        size={100}
-        color="red"
-      /> */}
+      <PuffLoader size={100} color="red" />
     </div>
   );
 };
