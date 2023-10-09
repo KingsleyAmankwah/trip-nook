78f9203c53c90c81652ba4f0604aca935f0d82b9
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-10-09 20:38:26 +0000

    fetching listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div>Hello Trip Nook</div>;
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
+export default async function Home() {
+  const listings = await getListings();
+  const isEmpty = true;
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
